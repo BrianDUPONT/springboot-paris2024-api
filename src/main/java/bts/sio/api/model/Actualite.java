a0d0d695db2ac0a1b27751dae246c73103df354c
BrianDUPONT <brian-dupont@hotmail.com>
2024-10-02 09:16:35 +0200

    Création du model Actualite avec deux ManyToOne vers epreuve et sport

---
+++ src/main/java/bts/sio/api/model/Actualite.java
@@ -0,0 +1,34 @@
+package bts.sio.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "actualite")
+public class Actualite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="titre")
+    private String titre;
+
+    @Column(name="contenu")
+    private String contenu;
+
+    @Column(name="date")
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "epreuve_id")
+    private Epreuve epreuve;
+
+    @ManyToOne
+    @JoinColumn(name = "sport_id")
+    private Sport sport;
+
+}
