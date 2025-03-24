71412d5593dbc9140af3667c3dcff435cd9be464
BrianDUPONT <brian-dupont@hotmail.com>
2025-03-24 14:35:22 +0100

    Création du model Niveau

---
+++ src/main/java/bts/sio/api/model/Niveau.java
@@ -0,0 +1,20 @@
+package bts.sio.api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "niveau")
+public class Niveau {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="libelle")
+    private String libelle;
+
+}
